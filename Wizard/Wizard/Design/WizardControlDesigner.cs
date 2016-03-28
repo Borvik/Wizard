b40925441f4d38a8881f336035b2257b21802ebb
Chris Kolkman <ckolkman@treorisoft.com>
2016-03-28 18:06:46 -0400

    Updated namespace.

--- Wizard/Wizard/Design/WizardControlDesigner.cs
+++ Wizard/Wizard/Design/WizardControlDesigner.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms.Design;
 
-namespace Treorisoft.Wizard.Designers
+namespace Treorisoft.Wizard.Design
 {
     internal class WizardControlDesigner : ParentControlDesigner
     {
--- Wizard/Wizard/WizardControl.cs
+++ Wizard/Wizard/WizardControl.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Treorisoft.Wizard.Designers;
+using Treorisoft.Wizard.Design;
 
 namespace Treorisoft.Wizard
 {
