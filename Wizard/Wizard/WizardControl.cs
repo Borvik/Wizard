6b24b7c3983ff0a198f47bf4561477fb67130df0
Chris Kolkman <ckolkman@treorisoft.com>
2016-03-28 08:00:20 -0400

    Control and designer start - also started the demo project.

--- Wizard/Wizard/WizardControl.cs
+++ Wizard/Wizard/WizardControl.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Treorisoft.Wizard.Designers;
 
 namespace Treorisoft.Wizard
 {
-    public class WizardControl
+    [Designer(typeof(WizardControlDesigner))]
+    public class WizardControl : Control
     {
+        public WizardControl()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
     }
 }
